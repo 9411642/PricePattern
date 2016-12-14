9febead707997e5d0e028a1ae3acbd8944a08f7c
derektu <derektu123@gmail.com>
2016-12-14 23:40:53 +0800

    ignore png file (created during testing)

--- test/test_rdp.py
+++ test/test_rdp.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     kdatasvc = kdata.KDataSvc("203.67.19.12")
     klist = kdatasvc.getdata("2330.TW", 8, date(2014, 1, 1), date(2015, 12, 31))
     r = rdp.RDP(klist, 3)
-    r.show()
+    r.render_png("test.png")
